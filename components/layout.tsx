8b3f421ea3b850c13f78e31d1e0aebb7b7788b74
Jeff Reiner <gone@hey.com>
2020-11-05 18:08:04 +0100

    Adding layout component

---
+++ components/layout.tsx
@@ -0,0 +1,16 @@
+import Meta from "./meta";
+
+export default function Layout({
+  preview,
+  children,
+}: {
+  preview: boolean;
+}): JSX.Element {
+  return (
+    <>
+      <Meta />
+
+      <main>{children}</main>
+    </>
+  );
+}
