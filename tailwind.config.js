8e1aba45e5d4923db3528a3cb364682869145cec
Jeff Reiner <gone@hey.com>
2020-11-05 17:41:49 +0100

    Adding Tailwind css directives to global files and config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./pages/**/*.js", "./pages/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
