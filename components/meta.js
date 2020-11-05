6034f91e9804928820c2848de3880060348ed42e
Jeff Reiner <gone@hey.com>
2020-11-05 18:02:15 +0100

    Stubbing in a simple Meta component

---
+++ components/meta.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Meta() {
+  return (
+    <Head>
+      <meta name="theme-color" content="#000" />
+      {/* Add Favicon & OG Meta Tags */}
+    </Head>
+  );
+}
