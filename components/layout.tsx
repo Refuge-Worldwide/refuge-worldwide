25941b1adac3048fde6ac5097c515337e3eb46b7
Jeff Reiner <gone@hey.com>
2020-11-05 18:12:51 +0100

    Adding custom type to Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,10 @@
 import Meta from "./meta";
 
-export default function Layout({
-  preview,
-  children,
-}: {
-  preview: boolean;
-}): JSX.Element {
+interface LayoutType extends JSX.Element {
+  preview?: boolean;
+}
+
+export default function Layout({ preview, children }): LayoutType {
   return (
     <>
       <Meta />
