254917ff42050350ecee8a43a2027661108ea0a7
Jeff Reiner <gone@hey.com>
2020-11-05 17:52:43 +0100

    Adding Types to _app and rename MyApp to RefugeApp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function RefugeApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default RefugeApp;
