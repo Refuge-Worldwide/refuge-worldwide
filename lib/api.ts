a8e5ec84788e1e3d1b755de3a6278e05a90c8837
Jeff Reiner <gone@hey.com>
2020-11-05 17:26:45 +0100

    Adding beginnings of Contentful API client

---
+++ lib/api.ts
@@ -0,0 +1,14 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+const previewClient = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  host: "preview.contentful.com",
+});
+
+const getClient = (preview: boolean) => (preview ? previewClient : client);
