2fb28eae9055eab41c79f44d93424073aca7e812
Jeff Reiner <gone@hey.com>
2020-11-12 15:35:48 +0100

    Adding more content to the footer

--- components/footer.js
+++ components/footer.js
@@ -2,22 +2,42 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
-      <h3>About Refuge</h3>
-      <p>
-        Refuge is a fundraising platform working in solidarity with grass- roots
-        and non-profit organizations.
-      </p>
-      <p>
-        Since 2015, we have donated over €50,000 EUR to community groups in
-        Berlin, by raising money at our basement parties, week- end-long raves,
-        and a street food festival.
-      </p>
-      <p>
-        <Link href="/about">
-          <a>Read more about us</a>
-        </Link>
-      </p>
+    <footer className="bg-black text-white">
+      <div>
+        <h3>About Refuge</h3>
+        <p>
+          Refuge is a fundraising platform working in solidarity with grass-
+          roots and non-profit organizations.
+        </p>
+        <p>
+          Since 2015, we have donated over €50,000 EUR to community groups in
+          Berlin, by raising money at our basement parties, week- end-long
+          raves, and a street food festival.
+        </p>
+        <p>
+          <Link href="/about">
+            <a>Read more about us</a>
+          </Link>
+        </p>
+      </div>
+
+      <div className="border-t border-white">
+        <p>
+          <a href="#top">Go up</a>
+        </p>
+
+        <p>
+          <Link href="/imprint">
+            <a>Imprint</a>
+          </Link>
+        </p>
+
+        <p>
+          <Link href="/privacy">
+            <a>Privacy</a>
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 }
