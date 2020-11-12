41133dda8dd02f7d7b4567591c29165f66ff3d68
Jeff Reiner <gone@hey.com>
2020-11-12 17:57:44 +0100

    Adding layout to the Player

--- components/player.tsx
+++ components/player.tsx
@@ -1,11 +1,16 @@
 export default function Player() {
   return (
-    <div className="bg-black text-white">
-      <p>Live</p>
-      <p>
-        Show Name this the name of a show Shuya Okino With ­Takeshi Yamaguchi,
-        ­Masaki Tamura, Sara Aiko
-      </p>
+    <div className="bg-black text-white grid grid-cols-10">
+      <div className="col-span-1">
+        <p>Live</p>
+      </div>
+      <div className="col-span-8">
+        <p>
+          Show Name this the name of a show Shuya Okino With ­Takeshi Yamaguchi,
+          ­Masaki Tamura, Sara Aiko
+        </p>
+      </div>
+      <div className="col-span-1">{/* Play / Pause */}</div>
     </div>
   );
 }
