ec42d8eea20fcf0b17dd25f06a05b394cf7d5953
Jeff Reiner <gone@hey.com>
2020-11-12 17:57:19 +0100

    Adding some layout to Footer & Navigation

--- components/footer.js
+++ components/footer.js
@@ -3,22 +3,26 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-black text-white">
-      <div>
-        <h3>About Refuge</h3>
-        <p>
-          Refuge is a fundraising platform working in solidarity with grass-
-          roots and non-profit organizations.
-        </p>
-        <p>
-          Since 2015, we have donated over €50,000 EUR to community groups in
-          Berlin, by raising money at our basement parties, week- end-long
-          raves, and a street food festival.
-        </p>
-        <p>
-          <Link href="/about">
-            <a>Read more about us</a>
-          </Link>
-        </p>
+      <div className="md:grid grid-cols-10">
+        <div className="col-span-1" />
+        <div className="col-span-4"></div>
+        <div className="col-span-4">
+          <h3>About Refuge</h3>
+          <p>
+            Refuge is a fundraising platform working in solidarity with grass-
+            roots and non-profit organizations.
+          </p>
+          <p>
+            Since 2015, we have donated over €50,000 EUR to community groups in
+            Berlin, by raising money at our basement parties, week-end-long
+            raves, and a street food festival.
+          </p>
+          <p>
+            <Link href="/about">
+              <a>Read more about us</a>
+            </Link>
+          </p>
+        </div>
       </div>
 
       <div className="border-t border-white">
--- components/navigation.js
+++ components/navigation.js
@@ -4,42 +4,46 @@ import NavigationLink from "./navigationLink";
 export default function Navigation() {
   return (
     <nav className="text-black">
-      <ul>
-        <li>
+      <ul className="grid grid-cols-10">
+        <li className="col-span-4">
           <Link href="/">
             <a>Refuge Worldwide</a>
           </Link>
         </li>
-        <li>
-          <NavigationLink href="/radio" activeClassName="text-orange">
-            Radio
-          </NavigationLink>
-        </li>
-        <li>
-          <NavigationLink href="/news" activeClassName="text-green">
-            News
-          </NavigationLink>
-        </li>
-        <li>
-          <NavigationLink href="/artists" activeClassName="text-purple">
-            Artists
-          </NavigationLink>
-        </li>
-        <li>
-          <NavigationLink href="/support" activeClassName="text-pink">
-            Support
-          </NavigationLink>
-        </li>
-        <li>
-          <NavigationLink href="/about" activeClassName="text-brown">
-            About
-          </NavigationLink>
-        </li>
-        <li>
-          <NavigationLink href="/newsletter" activeClassName="text-blue">
-            Newsletter
-          </NavigationLink>
-        </li>
+        <li className="col-span-6">
+          <ul className="flex justify-between">
+            <li>
+              <NavigationLink href="/radio" activeClassName="text-orange">
+                Radio
+              </NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/news" activeClassName="text-green">
+                News
+              </NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/artists" activeClassName="text-purple">
+                Artists
+              </NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/support" activeClassName="text-pink">
+                Support
+              </NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/about" activeClassName="text-brown">
+                About
+              </NavigationLink>
+            </li>
+            <li>
+              <NavigationLink href="/newsletter" activeClassName="text-blue">
+                Newsletter
+              </NavigationLink>
+            </li>
+          </ul>
+        </li>{" "}
       </ul>
     </nav>
   );
