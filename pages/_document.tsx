4c66a30705ba85ee1b9c6fb55cd38da1f58f4939
Jeff Reiner <gone@hey.com>
2020-11-12 17:57:36 +0100

    Antialiasing all text, setting font-size on the body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class RefugeDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="antialiased">
         <Head />
-        <body>
+        <body className="text-3xl">
           <Main />
           <NextScript />
         </body>
