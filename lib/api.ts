68ba63982af243c7ccf952cbf79df641fd57935c
Jeff Reiner <gone@hey.com>
2020-11-12 14:30:44 +0100

    Rework to use GraphQL for the API instead

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,17 @@
-import { createClient } from "contentful";
-
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-});
-
-const previewClient = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-  host: "preview.contentful.com",
-});
-
-const getClient = (preview: boolean) => (preview ? previewClient : client);
+export async function fetchGraphQL(query: string, preview = false) {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((response) => response.json());
+}
