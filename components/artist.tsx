63c29ad69d16ad8d08b481b73e8ad4d23c2a2d81
Jeff Reiner <gone@hey.com>
2020-11-18 12:05:44 +0100

    Updating artist image with not skewed sizes Updating typo in footer

--- components/artist.tsx
+++ components/artist.tsx
@@ -4,7 +4,13 @@ import type { ArtistInterface } from "../types/shared";
 export default function Artist({ name, photo }: ArtistInterface) {
   return (
     <div>
-      <Image src={photo.url} width={340} height={192} alt={name} />
+      <Image
+        className="object-cover object-center"
+        src={photo.url}
+        width={340}
+        height={192}
+        alt={name}
+      />
       <p>{name}</p>
     </div>
   );
--- components/footer.js
+++ components/footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
         <div className="col-span-4">
           <h3>About Refuge</h3>
           <p>
-            Refuge is a fundraising platform working in solidarity with grass-
-            roots and non-profit organizations.
+            Refuge is a fundraising platform working in solidarity with
+            grass-roots and non-profit organizations.
           </p>
           <p>
             Since 2015, we have donated over €50,000 EUR to community groups in
