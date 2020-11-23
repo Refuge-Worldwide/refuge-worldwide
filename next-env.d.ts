d38d9c276d489e4779eb4dda2b9e59d618214472
Jeff Reiner <gone@hey.com>
2020-11-23 15:27:20 +0100

    Typing things

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,19 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface FooterWidgetOptions {
+  disablePushstate: boolean;
+  disableUnloadWarning: boolean;
+  disableHotkeys: boolean;
+  hide_artwork: boolean;
+  light: boolean;
+  autoplay: boolean;
+}
+
+export declare global {
+  interface Window {
+    Mixcloud: {
+      FooterWidget: (showKey: string, FooterWidgetOptions) => Promise<{}>;
+    };
+  }
+}
