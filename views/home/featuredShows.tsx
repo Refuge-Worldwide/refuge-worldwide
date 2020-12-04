e32eb989c1dc8a8812548119d20295a0ac551156
Jeff Reiner <gone@hey.com>
2020-12-04 13:29:06 +0100

    Some more layout

--- views/home/featuredShows.tsx
+++ views/home/featuredShows.tsx
@@ -6,23 +6,29 @@ import { ShowInterface } from "../../types/shared";
 export default function FeaturedShows({ shows }: { shows: ShowInterface[] }) {
   return (
     <section>
-      <Pill>
-        <h2>Featured Shows</h2>
-      </Pill>
+      <div className="px-8 py-10">
+        <Pill>
+          <h2 className="px-6 py-3 leading-none">Featured Shows</h2>
+        </Pill>
 
-      <ul>
-        {shows.map((show, i) => (
-          <li key={i}>{show.title}</li>
-        ))}
-      </ul>
+        <div className="h-8" />
 
-      <div className="text-center">
-        <Link href="/radio">
-          <a className="inline-flex items-center space-x-4 font-medium">
-            <span className="underline">All Shows</span>
-            <Arrow />
-          </a>
-        </Link>
+        <ul>
+          {shows.map((show, i) => (
+            <li key={i}>{show.title}</li>
+          ))}
+        </ul>
+
+        <div className="h-16" />
+
+        <div className="text-center">
+          <Link href="/radio">
+            <a className="inline-flex items-center space-x-4 font-medium">
+              <span className="underline">All Shows</span>
+              <Arrow />
+            </a>
+          </Link>
+        </div>
       </div>
     </section>
   );
--- views/home/latestNews.tsx
+++ views/home/latestNews.tsx
@@ -6,23 +6,29 @@ import { ArticleInterface } from "../../types/shared";
 export default function LatestNews({ news }: { news: ArticleInterface[] }) {
   return (
     <section>
-      <Pill>
-        <h2>News</h2>
-      </Pill>
+      <div className="px-8 py-10">
+        <Pill>
+          <h2 className="px-6 py-3 leading-none">News</h2>
+        </Pill>
 
-      <ul>
-        {news.map((article, i) => (
-          <li key={i}>{article.title}</li>
-        ))}
-      </ul>
+        <div className="h-8" />
 
-      <div className="text-center">
-        <Link href="/news">
-          <a className="inline-flex items-center space-x-4 font-medium">
-            <span className="underline">All News</span>
-            <Arrow />
-          </a>
-        </Link>
+        <ul>
+          {news.map((article, i) => (
+            <li key={i}>{article.title}</li>
+          ))}
+        </ul>
+
+        <div className="h-16" />
+
+        <div className="text-center">
+          <Link href="/news">
+            <a className="inline-flex items-center space-x-4 font-medium">
+              <span className="underline">All News</span>
+              <Arrow />
+            </a>
+          </Link>
+        </div>
       </div>
     </section>
   );
