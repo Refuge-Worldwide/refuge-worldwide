35ab650215061abcd4f45bf0c61edce28753693a
Jeff Reiner <gone@hey.com>
2020-12-07 15:56:34 +0100

    More grid and spacing

--- views/radio/allShows.tsx
+++ views/radio/allShows.tsx
@@ -8,28 +8,34 @@ export default function AllShows() {
 
   return (
     <section>
-      <Pill>
-        <h2>All Shows</h2>
-      </Pill>
+      <div className="p-8">
+        <Pill>
+          <h2>All Shows</h2>
+        </Pill>
 
-      <ul className="w-full flex flex-wrap leading-none -mr-2 -mb-2">
-        <li className="inline-flex pr-2 pb-2">
-          <Badge invert text={"All"} />
-        </li>
-        {data?.genres?.map((genre, i) => (
-          <li className="inline-flex pr-2 pb-2" key={i}>
-            <Badge text={genre.name} />
-          </li>
-        ))}
-      </ul>
+        <div className="h-8" />
 
-      <ul className="flex">
-        {data?.shows?.map((show, i) => (
-          <li key={i}>
-            <ShowPreview {...show} />
+        <ul className="w-full flex flex-wrap leading-none -mr-2 -mb-2">
+          <li className="inline-flex pr-2 pb-2">
+            <Badge invert text={"All"} />
           </li>
-        ))}
-      </ul>
+          {data?.genres?.map((genre, i) => (
+            <li className="inline-flex pr-2 pb-2" key={i}>
+              <Badge text={genre.name} />
+            </li>
+          ))}
+        </ul>
+
+        <div className="h-4" />
+
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+          {data?.shows?.map((show, i) => (
+            <li key={i}>
+              <ShowPreview {...show} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
--- views/radio/nextShows.tsx
+++ views/radio/nextShows.tsx
@@ -2,10 +2,12 @@ import Pill from "../../components/pill";
 
 export default function NextShows() {
   return (
-    <section className="bg-orange">
-      <Pill>
-        <h2>Next Shows</h2>
-      </Pill>
+    <section className="bg-orange border-2">
+      <div className="px-8 py-6">
+        <Pill>
+          <h2>Next Shows</h2>
+        </Pill>
+      </div>
     </section>
   );
 }
