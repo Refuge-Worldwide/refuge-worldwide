09c9af3c3ca00410d6621c2e750f813f8550b57a
Jeff Reiner <gone@hey.com>
2020-12-07 15:56:05 +0100

    Adding featured news slider to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-import { getFeaturedShows, getLatestArticles } from "../lib/api";
+import {
+  getFeaturedArticles,
+  getFeaturedShows,
+  getLatestArticles,
+} from "../lib/api";
 import { ArticleInterface, ShowInterface } from "../types/shared";
 import FeaturedShows from "../views/home/featuredShows";
 import LatestNews from "../views/home/latestNews";
+import FeaturedArticles from "../views/news/featuredArticles";
 
 interface Page extends JSX.Element {
   preview: boolean;
   featuredShows: ShowInterface[];
   latestArticles: ArticleInterface[];
+  featuredArticles: ArticleInterface[];
 }
 
 export default function HomePage({
   preview,
   featuredShows,
   latestArticles,
+  featuredArticles,
 }: Page) {
   return (
     <Layout preview={preview}>
@@ -24,9 +31,7 @@ export default function HomePage({
 
       <FeaturedShows shows={featuredShows} />
 
-      <section>
-        <h2>SLIDER HERE</h2>
-      </section>
+      <FeaturedArticles articles={featuredArticles} />
 
       <LatestNews articles={latestArticles} />
     </Layout>
@@ -39,6 +44,7 @@ export async function getStaticProps({ preview = false }) {
       preview,
       featuredShows: await getFeaturedShows(preview),
       latestArticles: await getLatestArticles(preview),
+      featuredArticles: await getFeaturedArticles(preview),
     },
   };
 }
