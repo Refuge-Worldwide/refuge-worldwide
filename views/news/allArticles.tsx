b2c237d2b78f991919433960285fb5a439d73b88
Jeff Reiner <gone@hey.com>
2020-12-07 14:46:34 +0100

    Responsive AllArticles

--- views/news/allArticles.tsx
+++ views/news/allArticles.tsx
@@ -8,7 +8,7 @@ export default function AllArticles({
 }) {
   return (
     <section className="p-8">
-      <ul className="grid grid-cols-3 gap-8">
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
         {articles?.map((article, i) => (
           <li key={i}>
             <ArticlePreview withType {...article} />
