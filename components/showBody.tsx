0e5c16637f8af167044a0a87d64cf1680beec048
Jeff Reiner <theundeadghost@gmail.com>
2020-12-09 17:13:57 +0100

    Working on show body

--- components/showBody.tsx
+++ components/showBody.tsx
@@ -54,31 +54,45 @@ export default function ShowBody({
     <Fragment>
       <button onClick={handlePlayShow}>Play</button>
 
-      <p className="text-small">
+      <p className="text-small text-center">
         <Date dateString={date} />
       </p>
 
-      <h1>{title}</h1>
+      <div className="h-6" />
 
-      <ul className="flex">
+      <h1 className="text-base sm:text-large text-center">{title}</h1>
+
+      <div className="h-6" />
+
+      <ul className="flex flex-wrap justify-center -mr-2 -mb-2">
         {genres.map((genre, i) => (
-          <li key={i}>
+          <li key={i} className="pr-2 pb-2">
             <Badge text={genre.name} />
           </li>
         ))}
       </ul>
 
-      <p className="font-medium">With {persons}</p>
+      <div className="h-6" />
 
-      <div className="prose sm:prose-lg">
+      <p className="font-medium text-center">With {persons}</p>
+
+      <div className="h-6" />
+
+      <div className="p-8 prose sm:prose-lg">
         {documentToReactComponents(content?.json)}
       </div>
 
-      <Pill>
-        <span className="font-serif">Persons</span>
-      </Pill>
+      <div className="h-28" />
 
-      <p className="font-medium">{persons}</p>
+      <section className="border-t-2 pt-8 px-8 pb-14">
+        <Pill>
+          <span className="font-serif">Persons</span>
+        </Pill>
+
+        <div className="h-8" />
+
+        <p className="font-medium">{persons}</p>
+      </section>
     </Fragment>
   );
 }
