8ff7844fe1b10e55b9f62f0667c67a3649aae60c
Jeff Reiner <theundeadghost@gmail.com>
2020-12-09 18:18:04 +0100

    Making BackButton responsive

--- components/backButton.tsx
+++ components/backButton.tsx
@@ -8,11 +8,14 @@ export default function BackButton() {
 
   return (
     <button
-      className="inline-flex items-center space-x-3 border-2 rounded-full bg-black text-white border-white shadow-pill-white focus:outline-none focus:ring-4"
+      className="inline-flex items-center py-3 pr-4 sm:pr-6 pl-4 space-x-3 border-2 rounded-full bg-black text-white border-white shadow-pill-white focus:outline-none focus:ring-4"
       onClick={handleOnClick}
     >
-      <Arrow size={24} className="mt-px transform rotate-180" />
-      <span>Back</span>
+      <Arrow
+        size={24}
+        className="h-5 w-5 sm:h-6 sm:w-6   transform rotate-180"
+      />
+      <span className="hidden sm:inline leading-6">Back</span>
     </button>
   );
 }
