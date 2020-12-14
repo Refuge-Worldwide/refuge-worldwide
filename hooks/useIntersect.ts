36bf6a6b085491b9256c852b2ffb08b0b27ee1e5
Jeff Reiner <theundeadghost@gmail.com>
2020-12-14 14:09:09 +0100

    Adding useIntersect hook

---
+++ hooks/useIntersect.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useIntersect({
+  root = null,
+  rootMargin,
+  threshold = 0,
+}: {
+  root?: Element;
+  rootMargin?: string;
+  threshold: number | number[];
+}) {
+  const [entry, updateEntry] = useState({});
+  const [node, setNode] = useState(null);
+
+  const observer = useRef(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new window.IntersectionObserver(
+      ([entry]) => updateEntry(entry),
+      {
+        root,
+        rootMargin,
+        threshold,
+      }
+    );
+
+    const { current: currentObserver } = observer;
+
+    if (node) currentObserver.observe(node);
+
+    return () => currentObserver.disconnect();
+  }, [node, root, rootMargin, threshold]);
+
+  return [setNode, entry];
+}
