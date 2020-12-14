f597c7b00791d5cbaa12400a0189315d4fcd7b6f
Jeff Reiner <theundeadghost@gmail.com>
2020-12-14 14:09:54 +0100

    Fix overflow with flex

--- views/home/featuredShows.tsx
+++ views/home/featuredShows.tsx
@@ -14,7 +14,7 @@ export default function FeaturedShows({ shows }: { shows: ShowInterface[] }) {
 
         <div className="h-8" />
 
-        <ul className="flex flex-wrap -mr-10 -mb-10">
+        <ul className="flex w-full flex-wrap -mr-10 -mb-10">
           {shows.map((show, i) => (
             <li key={i} className="pr-10 pb-10">
               <ShowPreview {...show} />
