25dfc97f88cb2444c279a6bc50e1d72031c5f14b
Jeff Reiner <theundeadghost@gmail.com>
2020-12-15 17:52:51 +0100

    Key

--- components/featuredArticlePreview.tsx
+++ components/featuredArticlePreview.tsx
@@ -66,6 +66,7 @@ export default function FeaturedArticlePreview({
 
       <div className="md:col-span-5 2xl:col-span-7 h-56 md:h-auto relative border-l-2 border-t-2 border-b-2 ">
         <Image
+          key={slug}
           src={coverImage.url}
           objectFit="cover"
           objectPosition="center"
