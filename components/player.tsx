cb3da583dc95ee33f9ad4a04e5891500a30c962e
Jeff Reiner <theundeadghost@gmail.com>
2020-12-16 17:28:43 +0100

    Player

--- components/player.tsx
+++ components/player.tsx
@@ -1,16 +1,23 @@
 export default function Player() {
   return (
-    <div className="bg-black text-white grid grid-cols-10">
-      <div className="col-span-1">
-        <p>Live</p>
+    <section className="bg-black text-white">
+      <div className="pt-2 pb-2 grid grid-cols-10">
+        <div className="col-span-1">
+          <div className="px-4 md:px-8">
+            <div className="flex items-center space-x-6">
+              <div className="flex-shrink-0 h-10 w-10 rounded-full bg-red" />
+              <p>Live</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-8">
+          <p>
+            Show Name this the name of a show Shuya Okino With ­Takeshi
+            Yamaguchi, ­Masaki Tamura, Sara Aiko
+          </p>
+        </div>
+        <div className="col-span-1">{/* Play / Pause */}</div>
       </div>
-      <div className="col-span-8">
-        <p>
-          Show Name this the name of a show Shuya Okino With ­Takeshi Yamaguchi,
-          ­Masaki Tamura, Sara Aiko
-        </p>
-      </div>
-      <div className="col-span-1">{/* Play / Pause */}</div>
-    </div>
+    </section>
   );
 }
