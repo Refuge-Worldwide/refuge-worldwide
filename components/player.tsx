56c4ae78efbc1d48bf3fff8ff2455544ce265dd7
Jeff Reiner <theundeadghost@gmail.com>
2020-12-22 11:36:56 +0100

    Player

--- components/player.tsx
+++ components/player.tsx
@@ -42,7 +42,7 @@ const BroadcastingIndicator = ({
 
   return (
     <div className="flex items-center space-x-6">
-      <div className="flex-shrink-0 h-10 w-10 rounded-full bg-red opacity-10 animate-pulse" />
+      <div className="flex-shrink-0 h-10 w-10 rounded-full bg-white opacity-25" />
       <p>Offline</p>
     </div>
   );
@@ -77,11 +77,11 @@ function usePlayerState(ref: MutableRefObject<HTMLAudioElement>) {
 
 export default function Player() {
   const REFUGE_WW = "s3699c5e49";
-  const FOUNDATION_FM = "s0628bdd53";
 
   const AUDIO_SRC = `https://streaming.radio.co/${REFUGE_WW}/listen`;
 
   const { data } = useRadioCoStatus(REFUGE_WW);
+  const isOnline = data?.status === "online";
 
   const player = useRef<HTMLAudioElement>(null);
 
@@ -89,40 +89,48 @@ export default function Player() {
 
   return (
     <section className="bg-black text-white">
-      <div className="mx-10 pt-3 pb-3 grid grid-cols-10 gap-9 items-center">
-        <div className="col-span-1">
-          <BroadcastingIndicator status={data?.status} />
-        </div>
-        <div className="col-span-8">
-          <div className="overflow-x-scroll">
-            <p className="whitespace-nowrap">{data?.current_track.title}</p>
+      <div className="px-4 md:px-8 pt-3 pb-3">
+        <div className="grid grid-cols-10 gap-9 items-center">
+          <div className="col-span-1">
+            <BroadcastingIndicator status={data?.status} />
           </div>
-        </div>
-        <div className="col-span-1">
-          <div className="flex justify-end">
-            {isPlaying ? (
-              <button
-                className="focus:outline-none focus:ring-4"
-                onClick={pause}
-              >
-                <Pause />
-              </button>
-            ) : (
-              <button
-                className="focus:outline-none focus:ring-4"
-                onClick={play}
-              >
-                <Play />
-              </button>
+          <div className="col-span-8">
+            {isOnline ? (
+              <div className="overflow-x-scroll">
+                <p className="whitespace-nowrap">{data?.current_track.title}</p>
+              </div>
+            ) : null}
+          </div>
+          <div className="col-span-1">
+            {isOnline && (
+              <div className="flex justify-end">
+                {isPlaying ? (
+                  <button
+                    className="focus:outline-none focus:ring-4"
+                    onClick={pause}
+                  >
+                    <Pause />
+                  </button>
+                ) : (
+                  <button
+                    className="focus:outline-none focus:ring-4"
+                    onClick={play}
+                  >
+                    <Play />
+                  </button>
+                )}
+              </div>
             )}
           </div>
         </div>
       </div>
 
-      <audio ref={player} src={AUDIO_SRC} hidden>
-        <source src={AUDIO_SRC} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      {isOnline && (
+        <audio ref={player} src={AUDIO_SRC} hidden>
+          <source src={AUDIO_SRC} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      )}
     </section>
   );
 }
