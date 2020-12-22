143f2c115d0a1a02c853e67356ba7fba1a1d5c72
Jeff Reiner <theundeadghost@gmail.com>
2020-12-22 15:48:12 +0100

    Fix back button

--- components/backButton.tsx
+++ components/backButton.tsx
@@ -8,13 +8,10 @@ export default function BackButton() {
 
   return (
     <button
-      className="inline-flex items-center py-3 pr-4 sm:pr-6 pl-4 space-x-3 border-2 rounded-full bg-black text-white border-white shadow-pill-white focus:outline-none focus:ring-4"
+      className="inline-flex items-center py-3 pr-4 sm:pr-6 pl-4 space-x-3 border-2 rounded-full bg-black text-white border-white shadow-pill-white focus:outline-none focus:ring-4 font-light"
       onClick={handleOnClick}
     >
-      <Arrow
-        size={24}
-        className="h-5 w-5 sm:h-6 sm:w-6   transform rotate-180"
-      />
+      <Arrow size={24} className="h-5 w-5 sm:h-6 sm:w-6 transform rotate-180" />
       <span className="hidden sm:inline leading-6">Back</span>
     </button>
   );
