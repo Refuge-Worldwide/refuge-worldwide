37d67ef5811fb49d09f56d24df46d5f9e0d4612a
Jeff Reiner <theundeadghost@gmail.com>
2020-12-22 14:09:27 +0100

    Working on nav

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,50 +1,85 @@
+import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import NavigationLink from "./navigationLink";
+import cn from "classnames";
+
+function useActivePath(path = "/") {
+  const { pathname } = useRouter();
+
+  return pathname === path;
+}
+
+function NavigationLogo() {
+  const isHome = useActivePath();
+
+  let image = <img src="/images/navigation-smile.svg" alt="Refuge" />;
+
+  if (isHome)
+    image = (
+      <img
+        src="/images/navigation-smile-full.svg"
+        alt="Refuge Social Clubbing Worldwild"
+      />
+    );
+
+  return (
+    <Link href="/">
+      <a>{image}</a>
+    </Link>
+  );
+}
 
 export default function Navigation() {
+  const isHome = useActivePath();
+
+  const contentWrapperClassNames = cn(
+    "px-4 md:px-8",
+    isHome ? "py-6" : "py-2.5"
+  );
+
   return (
     <nav className="text-black">
-      <ul className="flex">
-        <li>
-          <Link href="/">
-            <a>Refuge Worldwide</a>
-          </Link>
-        </li>
-        <li className="flex-1">
-          <ul className="md:flex justify-end space-x-6 lg:space-x-8 xl:space-x-14">
-            <li>
-              <NavigationLink href="/radio" activeClassName="text-orange">
-                Radio
-              </NavigationLink>
-            </li>
-            <li>
-              <NavigationLink href="/news" activeClassName="text-green">
-                News
-              </NavigationLink>
-            </li>
-            <li>
-              <NavigationLink href="/artists" activeClassName="text-purple">
-                Artists
-              </NavigationLink>
-            </li>
-            <li>
-              <NavigationLink href="/support" activeClassName="text-pink">
-                Support
-              </NavigationLink>
-            </li>
-            <li>
-              <NavigationLink href="/about" activeClassName="text-brown">
-                About
-              </NavigationLink>
-            </li>
-            <li>
-              <NavigationLink href="/newsletter" activeClassName="text-blue">
-                Newsletter
-              </NavigationLink>
-            </li>
-          </ul>
-        </li>{" "}
-      </ul>
+      <div className={contentWrapperClassNames}>
+        <ul className="flex items-center">
+          <li>
+            <NavigationLogo />
+          </li>
+          <li className="flex-1">
+            <ul className="md:flex justify-end space-x-6 lg:space-x-8 xl:space-x-14">
+              <li>
+                <NavigationLink href="/radio" activeClassName="text-orange">
+                  Radio
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href="/news" activeClassName="text-green">
+                  News
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href="/artists" activeClassName="text-purple">
+                  Artists
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href="/support" activeClassName="text-pink">
+                  Support
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href="/about" activeClassName="text-brown">
+                  About
+                </NavigationLink>
+              </li>
+              <li>
+                <NavigationLink href="/newsletter" activeClassName="text-blue">
+                  Newsletter
+                </NavigationLink>
+              </li>
+            </ul>
+          </li>{" "}
+        </ul>
+      </div>
     </nav>
   );
 }
