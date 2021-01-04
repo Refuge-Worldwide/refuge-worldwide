abc75f4a0397df73a775259b6c1916e37ba19bae
Jeff Reiner <theundeadghost@gmail.com>
2021-01-04 10:03:58 +0100

    Lazy load LivePlayer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import Footer from "../components/footer";
-import LivePlayer from "../components/livePlayer";
 import Meta from "../components/meta";
 import Navigation from "../components/navigation";
 import "../styles/globals.css";
 
 const MixcloudPlayer = dynamic(() => import("../components/mixcloudPlayer"), {
+  ssr: false,
+});
+
+const LivePlayer = dynamic(() => import("../components/livePlayer"), {
   ssr: false,
 });
 
