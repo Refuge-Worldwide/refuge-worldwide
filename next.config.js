75b190b564bdb2c4a92c3b5e726a8bf84ed51f8b
Jeff Reiner <theundeadghost@gmail.com>
2021-01-04 15:17:24 +0100

    Adding analyze

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ["images.ctfassets.net"],
   },
   reactStrictMode: true,
-};
+});
