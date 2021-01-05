ad451e5f20fb47f4979b1e24a6149b32c8298a8a
Jeff Reiner <theundeadghost@gmail.com>
2021-01-05 18:43:29 +0100

    Working on SinglePage component

--- components/singlePage.tsx
+++ components/singlePage.tsx
@@ -14,24 +14,21 @@ export default function SinglePage({
 }) {
   return (
     <Fragment>
-      <div className="relative">
+      <div className="relative h-56 sm:h-96">
         {withBackButton && (
           <div className="absolute left-4 sm:left-8 top-3 sm:top-4 z-10">
             <BackButton />
           </div>
         )}
 
-        <div className="flex">
-          <Image
-            key={coverImage.title}
-            src={coverImage.url}
-            alt={coverImage.title}
-            width={640}
-            height={360}
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </div>
+        <Image
+          key={coverImage.title}
+          src={coverImage.url}
+          alt={coverImage.title}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
       </div>
 
       <div>{children}</div>
