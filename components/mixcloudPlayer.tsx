07889f1c38898a89e7411f98515c62d9ad0b46bb
Jeff Reiner <theundeadghost@gmail.com>
2021-01-05 11:49:40 +0100

    Setting autoplay attribute on the Mixcloud iframe embed url

--- components/mixcloudPlayer.tsx
+++ components/mixcloudPlayer.tsx
@@ -72,6 +72,7 @@ export default function MixcloudPlayer({ mini = true }: { mini?: boolean }) {
         src={
           `https://www.mixcloud.com/widget/iframe/?` +
           `hide_cover=1&` +
+          `autoplay=1&` +
           `light=1&` +
           `mini=${mini ? 1 : 0}&` +
           `feed=${key}`
