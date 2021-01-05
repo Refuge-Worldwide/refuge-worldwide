17623c2c17f5dedef243b79bfab26ad05be0bd6b
Jeff Reiner <theundeadghost@gmail.com>
2021-01-05 13:57:23 +0100

    Clean up Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,10 @@
 export default function Layout({
-  preview,
-  className = "",
+  className,
   children,
 }: {
   children: React.ReactNode;
   preview?: boolean;
   className?: string;
 }) {
-  return (
-    <>
-      <main className={className}>{children}</main>
-    </>
-  );
+  return <main className={className}>{children}</main>;
 }
