887dd49dce7860d420640322160b9bcfd19c5f05
Jeff Reiner <theundeadghost@gmail.com>
2021-01-11 11:45:06 +0100

    FeaturedArticle is now all clickable

--- components/featuredArticlePreview.tsx
+++ components/featuredArticlePreview.tsx
@@ -1,3 +1,4 @@
+import cn from "classnames";
 import Image from "next/image";
 import Link from "next/link";
 import { Arrow } from "../icons/arrow";
@@ -25,62 +26,65 @@ export default function FeaturedArticlePreview({
   articleType,
   coverImage,
 }: ArticleInterface) {
-  const articleClassName = getArticleBackgroundColor(articleType);
+  const articleBackgroundClassName = getArticleBackgroundColor(articleType);
+
+  const articleClassNames = cn(
+    "flex flex-col-reverse md:grid grid-cols-10 h-full",
+    articleBackgroundClassName
+  );
 
   return (
-    <article
-      className={
-        "flex flex-col-reverse md:grid grid-cols-10 h-full " + articleClassName
-      }
-    >
-      <header className="flex-1 md:col-span-5 2xl:col-span-3 p-4 lg:p-8 border-l-2 border-t-0 md:border-t-2 border-b-2">
-        <Pill>
-          <span className="font-serif">{articleType}</span>
-        </Pill>
+    <Link href={`/news/${slug}`}>
+      <a aria-labelledby={`featured-article-${slug}`}>
+        <article className={articleClassNames}>
+          <header className="flex-1 md:col-span-5 2xl:col-span-3 p-4 lg:p-8 border-l-2 border-t-0 md:border-t-2 border-b-2">
+            <Pill>
+              <span className="font-serif">{articleType}</span>
+            </Pill>
 
-        <div className="h-3 sm:h-6" />
+            <div className="h-3 sm:h-6" />
 
-        <p className="font-medium">
-          <Date dateString={date} />
-        </p>
+            <p className="font-medium">
+              <Date dateString={date} />
+            </p>
 
-        <div className="h-2" />
+            <div className="h-2" />
 
-        <h1 id={`featured-article-${slug}`} className="text-base sm:text-large">
-          {title}
-        </h1>
+            <h1
+              id={`featured-article-${slug}`}
+              className="text-base sm:text-large"
+            >
+              {title}
+            </h1>
 
-        <div className="h-4" />
+            <div className="h-4" />
 
-        <p className="font-medium">{subtitle}</p>
+            <p className="font-medium">{subtitle}</p>
 
-        <div className="h-6" />
+            <div className="h-6" />
 
-        <Link href={`/news/${slug}`}>
-          <a
-            className="inline-flex items-center space-x-5 font-medium leading-none focus:outline-none"
-            aria-labelledby={`featured-article-${slug}`}
-          >
-            <span className="underline">Read more</span>
-            <Arrow />
-          </a>
-        </Link>
+            <div className="inline-flex items-center space-x-5 font-medium leading-none ">
+              <span className="underline">Read more</span>
+              <Arrow />
+            </div>
 
-        <div className="hidden sm:block h-6" />
-      </header>
+            <div className="hidden sm:block h-6" />
+          </header>
 
-      <div className="md:col-span-5 2xl:col-span-7 h-56 md:h-auto relative border-l-2 border-t-2 border-b-2">
-        <Image
-          key={slug}
-          alt={coverImage.title}
-          src={coverImage.url}
-          objectFit="cover"
-          objectPosition="center"
-          layout="fill"
-          loading="eager"
-          priority
-        />
-      </div>
-    </article>
+          <div className="md:col-span-5 2xl:col-span-7 h-56 md:h-auto relative border-l-2 border-t-2 border-b-2">
+            <Image
+              key={slug}
+              alt={coverImage.title}
+              src={coverImage.url}
+              objectFit="cover"
+              objectPosition="center"
+              layout="fill"
+              loading="eager"
+              priority
+            />
+          </div>
+        </article>
+      </a>
+    </Link>
   );
 }
