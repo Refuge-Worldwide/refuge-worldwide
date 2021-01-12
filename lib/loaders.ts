397cb2268f294e59215236f477d5f2df09355247
Jeff Reiner <theundeadghost@gmail.com>
2021-01-12 16:15:54 +0100

    Remove log

--- lib/loaders.ts
+++ lib/loaders.ts
@@ -1,6 +1,5 @@
 import { ImageLoaderProps } from "next/image";
 
 export function contentful({ src, width, quality = 75 }: ImageLoaderProps) {
-  console.log(width);
   return `${src}?w=${width}&q=${quality}&fm=jpg&fl=progressive`;
 }
