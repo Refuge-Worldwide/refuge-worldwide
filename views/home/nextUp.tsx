315b5d57e502fdaa4b806712f67b92973b7f4499
Jeff Reiner <theundeadghost@gmail.com>
2021-01-12 16:38:24 +0100

    Don't collapse orange bar to a stack

--- views/home/nextUp.tsx
+++ views/home/nextUp.tsx
@@ -6,14 +6,14 @@ export default function NextUp({ content, header }: NextUpSection) {
   if (content.json && typeof header === "string")
     return (
       <section className="bg-orange border-t-2 border-b-2">
-        <div className="md:flex">
-          <div className="pt-4 pb-4 px-4 md:px-8 md:border-r-2">
+        <div className="flex items-center">
+          <div className="pt-4 pb-4 px-4 md:px-8 border-r-2">
             <Pill size="medium">
               <h2 className="whitespace-nowrap">{header}</h2>
             </Pill>
           </div>
-          <div className="md:pt-4 pb-4 px-4 md:px-0 flex items-center overflow-x-scroll">
-            <span className="md:flex md:space-x-2 md:whitespace-nowrap md:px-4">
+          <div className="pt-4 pb-4 overflow-x-scroll hide-scrollbar">
+            <span className="h-10 flex items-center space-x-2 whitespace-nowrap px-4">
               {documentToReactComponents(content?.json)}
             </span>
           </div>
