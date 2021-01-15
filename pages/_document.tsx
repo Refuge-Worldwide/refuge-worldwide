a5cbc66b0b4af53d30b8bb544ca16e24a5fe6d8e
Jeff Reiner <theundeadghost@gmail.com>
2021-01-15 15:25:01 +0100

    Adding custom favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,8 +32,26 @@ class RefugeDocument extends Document {
             as="style"
           />
           <link rel="stylesheet" href="https://use.typekit.net/rqu7mun.css" />
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="theme-color" content="#000" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
         </Head>
         <body className="text-small sm:text-base font-light">
           <Main />
