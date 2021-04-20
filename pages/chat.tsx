74836bd118c86aa6ecdc47c31d77f3f5beb3d4c2
Jeff Reiner <theundeadghost@gmail.com>
2021-04-20 19:17:03 +0200

    Finish adding in chat page

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,16 +1,45 @@
+import { useEffect, useRef } from "react";
 import PageMeta from "../components/seo/page";
-import dynamic from "next/dynamic";
-
-const Chatango = dynamic(() => import("../components/chatango"), {
-  ssr: false,
-});
 
 export default function ChatPage() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <PageMeta title="Chat | Refuge Worldwide" path="chat/" />
+  const ref = useRef<HTMLDivElement>();
 
-      <Chatango />
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const tag = document.createElement("script");
+
+    tag.async = false;
+    tag.defer = true;
+    tag.id = "cid0020000279925137861";
+    tag.src = "https://st.chatango.com/js/gz/emb.js";
+    tag.innerText = JSON.stringify({
+      handle: "refugeworldwidechat",
+      arch: "js",
+      styles: {
+        a: "000000",
+        b: 100,
+        c: "FFFFFF",
+        d: "FFFFFF",
+        k: "000000",
+        l: "000000",
+        m: "000000",
+        n: "FFFFFF",
+        p: "10",
+        q: "000000",
+        r: 100,
+        allowpm: 0,
+      },
+    });
+
+    ref.current.appendChild(tag);
+  });
+
+  return (
+    <div ref={ref} className="flex flex-col min-h-screen">
+      <PageMeta title="Chat | Refuge Worldwide" path="chat/" />
 
       <style jsx global>{`
         iframe {
