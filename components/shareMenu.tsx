f020860f3b8e97a7e876aea20d13cb77a9c56192
Jeff Reiner <theundeadghost@gmail.com>
2021-09-01 17:38:43 +0200

    Refactor ShareMenu

--- components/shareMenu.tsx
+++ components/shareMenu.tsx
@@ -1,4 +1,5 @@
-import { Menu, MenuButton, MenuLink, MenuList } from "@reach/menu-button";
+import { Menu } from "@headlessui/react";
+import classNames from "classnames";
 import Share from "../icons/share";
 
 export default function ShareMenu({ url }: { url: string }) {
@@ -6,45 +7,76 @@ export default function ShareMenu({ url }: { url: string }) {
     "Refuge Worldwide is a community radio station and fundraising platform based in Berlin.";
 
   return (
-    <Menu>
-      <MenuButton className="w-20 h-20 sm:w-28 sm:h-28 focus:outline-none">
-        <Share />
-      </MenuButton>
-      <MenuList>
-        <WhatsApp link={url} text={TEXT} />
-        <Facebook link={url} />
-        <Twitter link={url} text={TEXT} />
-        <Telegram link={url} text={TEXT} />
-      </MenuList>
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="w-20 h-20 sm:w-28 sm:h-28 focus:outline-none">
+          <Share />
+        </Menu.Button>
+      </div>
+
+      <Menu.Items className="absolute left-0 mt-2 bg-black text-white focus:outline-none">
+        <div className="pt-4 pb-4">
+          <Menu.Item>
+            {({ active }) => (
+              <WhatsApp link={url} text={TEXT} active={active} />
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => <Facebook link={url} active={active} />}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => <Twitter link={url} text={TEXT} active={active} />}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Telegram link={url} text={TEXT} active={active} />
+            )}
+          </Menu.Item>
+        </div>
+      </Menu.Items>
     </Menu>
   );
 }
 
-const WhatsApp = ({ link, text }) => (
-  <MenuLink
-    className="hover:text-social-whatsapp"
+const menuItemClasses = "block text-small font-medium py-2 px-6";
+
+const WhatsApp = ({ link, text, active = false }) => (
+  <a
+    className={classNames(
+      menuItemClasses,
+      "hover:text-social-whatsapp",
+      active ? "text-social-whatsapp" : ""
+    )}
     target="_blank"
     rel="noopener noreferrer"
     href={`https://wa.me/?text=${encodeURI(`${text} ${link}`)}`}
   >
     WhatsApp
-  </MenuLink>
+  </a>
 );
 
-const Facebook = ({ link }) => (
-  <MenuLink
-    className="hover:text-social-facebook"
+const Facebook = ({ link, active = false }) => (
+  <a
+    className={classNames(
+      menuItemClasses,
+      "hover:text-social-facebook",
+      active ? "text-social-facebook" : ""
+    )}
     target="_blank"
     rel="noopener noreferrer"
     href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURI(link)}`}
   >
     Facebook
-  </MenuLink>
+  </a>
 );
 
-const Twitter = ({ link, text }) => (
-  <MenuLink
-    className="hover:text-social-twitter"
+const Twitter = ({ link, text, active = false }) => (
+  <a
+    className={classNames(
+      menuItemClasses,
+      "hover:text-social-twitter",
+      active ? "text-social-twitter" : ""
+    )}
     target="_blank"
     rel="noopener noreferrer"
     href={`https://twitter.com/intent/tweet?text=${encodeURI(
@@ -52,12 +84,16 @@ const Twitter = ({ link, text }) => (
     )}&url=${encodeURI(link)}&via=refugeworldwide`}
   >
     Twitter
-  </MenuLink>
+  </a>
 );
 
-const Telegram = ({ link, text }) => (
-  <MenuLink
-    className="hover:text-social-telegram"
+const Telegram = ({ link, text, active = false }) => (
+  <a
+    className={classNames(
+      menuItemClasses,
+      "hover:text-social-telegram",
+      active ? "text-social-telegram" : ""
+    )}
     target="_blank"
     rel="noopener noreferrer"
     href={`https://telegram.me/share/url?url=${encodeURI(
@@ -65,5 +101,5 @@ const Telegram = ({ link, text }) => (
     )}&text=${encodeURI(text)}`}
   >
     Telegram
-  </MenuLink>
+  </a>
 );
