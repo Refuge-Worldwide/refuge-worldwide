ae72bd68f62e27fe97132037de1e6a47a98e91fc
Jeff Reiner <theundeadghost@gmail.com>
2021-09-14 10:39:42 +0200

    Don't prefetch artist pages

--- components/artistPreview.tsx
+++ components/artistPreview.tsx
@@ -9,7 +9,7 @@ export default function ArtistPreview({
   slug,
 }: Pick<AllArtistEntry, "name" | "photo" | "slug">) {
   return (
-    <Link href={`/artists/${slug}`}>
+    <Link href={`/artists/${slug}`} prefetch={false}>
       <a>
         <Image
           key={photo.sys.id}
