ce5bc8803149b2377d04ae2a0cd6f017bb66233b
Jeff Reiner <theundeadghost@gmail.com>
2021-09-29 14:46:19 +0200

    Enabling Preview Mode

---
+++ pages/api/preview/radio.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getRadioPageSingle } from "../../../lib/contentful/pages/radio";
+
+export default async function preview(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { secret, slug } = req.query;
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    const { show } = await getRadioPageSingle(slug as string, true);
+
+    res.setPreviewData({});
+
+    res.writeHead(307, { Location: `/radio/${show.slug}` });
+    res.end();
+  } catch (error) {
+    console.error(error);
+
+    return res.status(401).json({ message: "Invalid slug" });
+  }
+}
