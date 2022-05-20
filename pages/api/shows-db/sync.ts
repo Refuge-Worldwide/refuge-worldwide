7202773719d4bad742c9734a1b3c9adfc96f0a7c
Jeff Reiner <theundeadghost@gmail.com>
2022-05-20 17:33:59 +0200

    Sync endpoint

---
+++ pages/api/shows-db/sync.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (process.env.CF_WEBHOOK_SECRET !== req.headers["webhook_secret"]) {
+    return res.status(401).json({ message: "Invalid Secret" });
+  }
+
+  try {
+    return res.json({});
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).send("Error Syncing");
+  }
+}
