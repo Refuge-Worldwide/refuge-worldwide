a4879191444a640713987471a9f40ab374ffe9e5
Jeff Reiner <theundeadghost@gmail.com>
2022-08-01 13:20:36 +0200

    Adding error handling

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,18 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { assertError } from "ts-extras";
 import { getSearchData } from "../../lib/contentful/search";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { query } = req.query as typeof req.query & {
-    query: string;
-  };
+  try {
+    const { query } = req.query as typeof req.query & {
+      query: string;
+    };
 
-  const { data, duration } = await getSearchData(query);
+    const { data, duration } = await getSearchData(query);
 
-  res
-    .setHeader("Server-Timing", `search;dur=${duration}`)
-    .setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59")
-    .json(data);
+    res
+      .setHeader("Server-Timing", `search;dur=${duration}`)
+      .setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59")
+      .json(data);
+  } catch (error) {
+    assertError(error);
+
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 }
