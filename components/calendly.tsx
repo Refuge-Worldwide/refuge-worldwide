3e3c5f84685ea8eb0b324005d17ab97c49693ea0
Jeff Reiner <theundeadghost@gmail.com>
2022-08-19 15:33:52 +0200

    Rework Calendly embed

--- components/calendly.tsx
+++ components/calendly.tsx
@@ -1,23 +1,23 @@
-import { useEffect } from "react";
-import useScript from "../hooks/useScript";
+import Script from "next/script";
 
 export default function CalendlyEmbed({ shouldInit }: { shouldInit: boolean }) {
-  const scriptStatus = useScript(
-    "//assets.calendly.com/assets/external/widget.js"
-  );
+  if (shouldInit) {
+    return (
+      <div id="calendly-embed" className="h-screen">
+        <Script
+          src="https://assets.calendly.com/assets/external/widget.js"
+          onLoad={() => {
+            (window as any).Calendly.initInlineWidget({
+              url: "https://calendly.com/studio-booking-refuge-worldwide/booking",
+              parentElement: document.getElementById("calendly-embed"),
+              prefill: {},
+              utm: {},
+            });
+          }}
+        />
+      </div>
+    );
+  }
 
-  useEffect(() => {
-    if (shouldInit === false || scriptStatus !== "ready") {
-      return;
-    }
-
-    (window as any).Calendly.initInlineWidget({
-      url: "https://calendly.com/studio-booking-refuge-worldwide/booking",
-      parentElement: document.getElementById("calendly-embed"),
-      prefill: {},
-      utm: {},
-    });
-  }, [scriptStatus, shouldInit]);
-
-  return <div id="calendly-embed" className="h-screen" />;
+  return null;
 }
