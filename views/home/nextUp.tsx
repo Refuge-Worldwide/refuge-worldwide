41b5f0cebce7c3b35fc7504935616db5ed55e8db
Jack MB <jackmb@Jacks-MacBook-Pro.local>
2023-01-20 12:33:22 +0100

    make pill outlined

--- views/home/nextUp.tsx
+++ views/home/nextUp.tsx
@@ -20,7 +20,7 @@ export default function NextUp({ content }: NextUpSection) {
       <section className={`${bgColour} border-t-2 border-b-2`}>
         <div className="flex items-center">
           <div className="pt-2 pb-2 sm:pt-4 sm:pb-4 px-4 md:px-8 border-r-2">
-            <Pill size="medium">
+            <Pill outline={true} size="medium">
               <h2 className="whitespace-nowrap">
                 Next <span className="hidden md:inline">Up</span>
               </h2>
