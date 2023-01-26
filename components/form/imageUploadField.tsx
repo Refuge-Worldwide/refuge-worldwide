dbab69c04e5eb50bb8b6d1c20d2d7384e27df42c
Jack MB <jackmb@Jacks-MacBook-Pro.local>
2023-01-26 10:49:05 +0100

    working on image upload field

---
+++ components/form/imageUploadField.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { FilePond, File, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+// import SingleLineField from './singleLineField'
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+// import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImagePreview);
+
+// Our app
+export default function ImageUploadField({
+  label,
+  name,
+  required,
+}: {
+  label: string;
+  name: string;
+  required?: boolean;
+}) {
+  const [files, setFiles] = useState([]);
+  return (
+    <div className="mb-10">
+      <label htmlFor={name}>
+        {label}
+        {required && "*"}
+      </label>
+      <FilePond
+        className="h-56"
+        files={files}
+        onupdatefiles={setFiles}
+        allowMultiple={false}
+        maxFiles={1}
+        server="/api/image-upload"
+        id={name}
+        name={name}
+        required={required}
+        credits={false}
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+      <label className="label-description" htmlFor="altText">
+        Description of image for accessibility*
+      </label>
+      <input
+        type="string"
+        id="altText"
+        name="altText"
+        className="pill-input"
+        required
+      />
+    </div>
+  );
+}
---
+++ pages/api/image-upload.ts
@@ -0,0 +1,44 @@
+import { createClient } from "contentful-management";
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_TOKEN;
+
+const client = createClient({
+  accessToken: accessToken,
+});
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const environment = "submission-sandbox";
+
+export default function handler(req, res) {
+  // Get data submitted in request's body.
+  const body = req.body;
+
+  // Optional logging to see the responses
+  // in the command line where next.js app is running.
+  console.log("body: ", body);
+
+  client
+    .getSpace(space)
+    .then((space) => space.getEnvironment(environment))
+    .then((environment) =>
+      environment.createAssetFromFiles({
+        fields: {
+          title: {
+            "en-US": name,
+          },
+          description: {
+            "en-US": alt,
+          },
+          file: {
+            "en-US": {
+              contentType: "image/jpg",
+              fileName: name + ".jpg",
+              file: image,
+            },
+          },
+        },
+      })
+    )
+    .then((asset) => asset.processForAllLocales())
+    .then((asset) => asset.publish())
+
+    .catch(console.error);
+}
