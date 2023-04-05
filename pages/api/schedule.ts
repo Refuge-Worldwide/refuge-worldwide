4f472a6f1e9d9220955ebf7a1f9fa64ad142be63
Jack MB <jack@designandcode.com>
2023-04-05 15:34:03 +0200

    use radio co-for live now

--- pages/api/schedule.ts
+++ pages/api/schedule.ts
@@ -36,22 +36,10 @@ export default async function handler(
     const r = await fetch("https://public.radio.co/stations/s3699c5e49/status");
     const radioCoData: RadioCo = await r.json();
 
-    let liveNow = {};
-    if (
-      radioCoData.current_track.title.startsWith("OVERWRITE: ") ||
-      !data.liveNow
-    ) {
-      liveNow = {
-        title: radioCoData.current_track.title.replace("OVERWRITE: ", ""),
-        artwork: radioCoData.current_track.artwork_url,
-      };
-    } else {
-      liveNow = {
-        title: data.liveNow.title,
-        artwork: radioCoData.current_track.artwork_url,
-        slug: data.liveNow.slug,
-      };
-    }
+    const liveNow = {
+      title: radioCoData.current_track.title,
+      artwork: radioCoData.current_track.artwork_url,
+    };
 
     const scheduleData = {
       status: radioCoData.status,
