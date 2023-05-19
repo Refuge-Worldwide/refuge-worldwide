dae8f320317b0a8e849d7c6c33225d92c1adc602
Jack MB <jack@designandcode.com>
2023-05-19 12:34:14 +0200

    correct ordering of months for upcoming events

--- views/events/upcomingEvents.tsx
+++ views/events/upcomingEvents.tsx
@@ -5,24 +5,26 @@ import EventRow from "../../components/eventRow";
 export default function UpcomingEvents({ events }) {
   return (
     <div className="border-t-2 bg-blue">
-      {Object.keys(events).map((month, index) => (
-        <div key={month}>
-          <div
-            className={`p-4 sm:p-8 sticky top-12 lg:top-14 border-b bg-blue ${
-              index > 0 ? "border-t" : ""
-            }`}
-          >
-            <Pill outline>
-              <h3>{month}</h3>
-            </Pill>
+      {Object.keys(events)
+        .reverse()
+        .map((month, index) => (
+          <div key={month}>
+            <div
+              className={`p-4 sm:p-8 sticky top-12 lg:top-14 border-b bg-blue ${
+                index > 0 ? "border-t" : ""
+              }`}
+            >
+              <Pill outline>
+                <h3>{month}</h3>
+              </Pill>
+            </div>
+            <ul>
+              {events[month].map((event) => (
+                <EventRow key={event.title} event={event} />
+              ))}
+            </ul>
           </div>
-          <ul>
-            {events[month].map((event) => (
-              <EventRow key={event.title} event={event} />
-            ))}
-          </ul>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
