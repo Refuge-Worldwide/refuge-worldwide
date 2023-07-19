80abb774f1304868275a53114d184e90defa6df9
Jack MB <jack@designandcode.com>
2023-07-19 10:40:53 +0200

    update submission success message

--- components/showSubmissionStep4.tsx
+++ components/showSubmissionStep4.tsx
@@ -1,9 +1,10 @@
 export default function ShowSubmissionStep4() {
   return (
     <div className="mb-64 mt-32">
-      <h3 className="font-sans  text-large font-medium text-center">
+      <h3 className="font-sans text-large font-medium text-center">
         Show submission successful,
-        <br /> we will be in touch soon.
+        <br /> we will be in touch with the artwork and more info on the day
+        before your show.
       </h3>
     </div>
   );
