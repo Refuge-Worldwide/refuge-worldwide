3bb93bfb2c847eb0c342862062972b4891e32a5b
Jack MB <jack@designandcode.com>
2023-09-05 16:22:49 +0200

    radio groups value selectable via keyboard

--- components/showSubmissionStep1.tsx
+++ components/showSubmissionStep1.tsx
@@ -1,12 +1,31 @@
-import { Field, ErrorMessage } from "formik";
+import { Field, ErrorMessage, useFormikContext } from "formik";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export default function ShowSubmissionStep1() {
+  const { setFieldValue } = useFormikContext();
   return (
     // We pass the event to the handleSubmit() function on submit.
     <div className="mt-16 mb-8">
       <fieldset>
         <legend>First up, is your show...*</legend>
-        <div className="flex">
+        <RadioGroup.Root
+          className="flex"
+          name="Type of show"
+          onValueChange={(value: string) => setFieldValue("showType", value)}
+        >
+          <RadioGroup.Item value="live" asChild>
+            <label className="data-[state=checked]:bg-orange block cursor-pointer pill-input rounded-tr-none rounded-br-none py-3 text-center">
+              Live
+            </label>
+          </RadioGroup.Item>
+
+          <RadioGroup.Item value="preRecord" asChild>
+            <label className="data-[state=checked]:bg-blue block cursor-pointer select-none pill-input rounded-tl-none rounded-bl-none py-3 text-center">
+              Pre-record
+            </label>
+          </RadioGroup.Item>
+        </RadioGroup.Root>
+        {/* <div className="flex">
           <div className="w-1/2">
             <Field
               type="radio"
@@ -37,7 +56,7 @@ export default function ShowSubmissionStep1() {
               Pre-recorded
             </label>
           </div>
-        </div>
+        </div> */}
         <ErrorMessage className="text-red" component="span" name="showType" />
       </fieldset>
     </div>
