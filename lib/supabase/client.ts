5a0ba8d45847e31c6e34c80b33793803e0d2e87d
Jack MB <jack@designandcode.com>
2023-09-19 16:28:28 +0200

    move supabase url to env

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
-export const supabase = createClient(
-  "https://oviucrnsbztpafeijwbt.supabase.co",
-  supabaseKey
-);
+export const supabase = createClient(supabaseURL, supabaseKey);
