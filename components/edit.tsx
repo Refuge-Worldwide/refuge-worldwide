2295744d243d3839d3d2128c947149248655d4d2
Jack MB <jack@designandcode.com>
2023-10-03 16:21:44 +0200

    adjust edit button on mobile homepage

--- components/edit.tsx
+++ components/edit.tsx
@@ -9,12 +9,12 @@ export default function Edit({ id }: { id?: string }) {
     return (
       <Link
         className={`absolute ${
-          pathname == "/" ? "mt-28" : "mt-4"
-        } right-6 rounded-full border border-black bg-white p-4 shadow-md z-10`}
+          pathname == "/" ? "mt-[58px] sm:mt-28 " : "mt-4"
+        } right-6 rounded-full border border-black bg-white p-3 shadow-md z-10`}
         href={`https://app.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/entries/${id}`}
         target="_blank"
       >
-        <AiOutlineEdit size={24} aria-label="Edit" />
+        <AiOutlineEdit size={20} aria-label="Edit" />
       </Link>
     );
 }
