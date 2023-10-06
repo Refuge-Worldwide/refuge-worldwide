94eaca6b2b1b95e22d59f9cab1ce8cd0db1d5ff7
Jack MB <jack@designandcode.com>
2023-10-06 10:10:33 +0200

    add axel arigato workshop page

--- lib/contentful/pages/tour.ts
+++ lib/contentful/pages/tour.ts
@@ -45,3 +45,47 @@ export async function getTourPage(preview: boolean) {
 
   return extractPage<TourPageData>(data, "pageTourWorkshopSignup");
 }
+
+export async function getAxelPage(preview: boolean) {
+  const AxelPageQuery = /* GraphQL */ `
+    query AxelPageQuery($preview: Boolean) {
+      pageTourWorkshopSignup(id: "6m5NBRavUFjf4sBUxFb9eb", preview: $preview) {
+        coverImage {
+          sys {
+            id
+          }
+          title
+          description
+          url
+          width
+          height
+        }
+        content {
+          json
+          links {
+            assets {
+              block {
+                sys {
+                  id
+                }
+                contentType
+                title
+                description
+                url
+                width
+                height
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await graphql(AxelPageQuery, {
+    variables: { preview },
+    preview,
+  });
+
+  return extractPage<TourPageData>(data, "pageTourWorkshopSignup");
+}
