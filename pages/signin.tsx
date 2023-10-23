af5d3003c6307e172af9146db5624fc6cb88bf3d
Jack MB <jack@designandcode.com>
2023-10-23 11:52:14 +0200

    remove console logs from signin

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -11,12 +11,11 @@ const LoginPage = () => {
 
   // if logged in then redirect to calendar
   if (user) {
-    // router.push("/admin/calendar");
+    router.push("/admin/calendar");
   }
 
   const getURL = () => {
     let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
       process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
       "http://localhost:3000/";
     // Make sure to include `https://` when not localhost.
@@ -25,9 +24,6 @@ const LoginPage = () => {
     url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
     return url;
   };
-
-  console.log("vercel url: " + process.env.NEXT_PUBLIC_VERCEL_URL);
-  console.log("url: " + getURL());
 
   return (
     <Layout>
