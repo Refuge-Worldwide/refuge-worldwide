eb5cfabc2757e293ad20f4404a854cb5e4289109
Jack MB <jack@designandcode.com>
2024-03-29 10:18:57 +0100

    manually change nextup banner for b2bday

--- views/home/nextUp.tsx
+++ views/home/nextUp.tsx
@@ -43,7 +43,8 @@ export default function NextUp() {
                   <span className="h-10 flex items-center space-x-2 whitespace-nowrap px-2">
                     {scheduleData.nextUp.map((show) => (
                       <p className="font-medium" key={show.title}>
-                        <LocalTime dateTime={show.date} /> {show.title}{" "}
+                        B2B Day{" "}
+                        {/* <LocalTime dateTime={show.date} /> {show.title}{" "} */}
                         &#47;&#47;{" "}
                       </p>
                     ))}
