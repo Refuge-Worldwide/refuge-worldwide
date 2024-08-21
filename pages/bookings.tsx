8322adea4d0796e4e46ec8d141238ca12cf35eff
Jack MB <hi@jackmurraybrown.com>
2024-08-21 15:02:40 +0200

    add booking pages for nm1 and oona dj

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -3,29 +3,36 @@ import BookingPasswordForm from "../components/bookingForm";
 import CalendlyEmbed from "../components/calendly";
 import Layout from "../components/layout";
 import PageMeta from "../components/seo/page";
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
 
 export default function BookingsPage() {
-  const [passwordCorrect, passwordCorrectSet] = useState(false);
+  // const [passwordCorrect, passwordCorrectSet] = useState(false);
 
-  const onPasswordCorrect = useCallback(() => {
-    passwordCorrectSet(true);
+  // const onPasswordCorrect = useCallback(() => {
+  //   passwordCorrectSet(true);
+  // }, []);
+
+  useEffect(() => {
+    (async function () {
+      const Cal = await getCalApi();
+      Cal("ui", {
+        theme: "light",
+        hideEventTypeDetails: false,
+      });
+    })();
   }, []);
 
   return (
     <Layout>
       <PageMeta title="Bookings | Refuge Worldwide" path="bookings/" />
 
-      {passwordCorrect ? (
-        <section>
-          <CalendlyEmbed shouldInit={passwordCorrect} />
-        </section>
-      ) : (
-        <section className="py-48 md:py-72">
-          <div className="container-md p-4 sm:p-8">
-            <BookingPasswordForm onPasswordCorrect={onPasswordCorrect} />
-          </div>
-        </section>
-      )}
+      <section className="">
+        <Cal
+          className="pt-24 pb-4 min-h-[75vh]"
+          calLink="refugeworldwide"
+        ></Cal>
+      </section>
     </Layout>
   );
 }
---
+++ pages/nm1/bookings.tsx
@@ -0,0 +1,37 @@
+import { InferGetStaticPropsType } from "next";
+import Layout from "../../components/layout";
+import Prose from "../../components/Prose";
+import PageMeta from "../../components/seo/page";
+import { getNM1BookingPage } from "../../lib/contentful/pages/nm1booking";
+import { RenderRichTextWithImages } from "../../lib/rich-text";
+import SinglePage from "../../views/singlePage";
+
+export async function getStaticProps({ preview = false }) {
+  return {
+    props: {
+      preview,
+      ...(await getNM1BookingPage(preview)),
+    },
+    revalidate: 60 * 60 * 24,
+  };
+}
+
+export default function AboutPage({
+  preview,
+  coverImage,
+  content,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout preview={preview} pageId="7nZvcNfNVRrZGs6x42RV3l">
+      <PageMeta title="NM1 Bookings | Refuge Worldwide" path="/nm1/bookings/" />
+
+      <SinglePage coverImage={coverImage}>
+        <section>
+          <div className="container-md p-4 sm:p-8 bg-white">
+            <Prose>{RenderRichTextWithImages(content)}</Prose>
+          </div>
+        </section>
+      </SinglePage>
+    </Layout>
+  );
+}
