aa01c612565069d787044ca679f07cfe5b5c79c5
Jack MB <hi@jackmurraybrown.com>
2024-11-19 15:40:49 +0100

    add back button to daily schedule generator

--- pages/admin/daily-schedule-generator.tsx
+++ pages/admin/daily-schedule-generator.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { TfiReload, TfiDownload } from "react-icons/tfi";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
+import BackButton from "../../components/backButton";
 const ScheduleArtworkPage: NextPage & {
   getLayout?: (page: React.ReactNode) => React.ReactNode;
 } = () => {
@@ -34,51 +34,28 @@ const ScheduleArtworkPage: NextPage & {
   }, []);
 
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div className="h-[80vh] flex items-center align-middle">
-        {imageUrl ? (
-          <img
-            className="border border-white"
-            src={imageUrl}
-            alt="Schedule Artwork"
-            style={{ maxWidth: "100%", maxHeight: "80vh" }}
-          />
-        ) : (
-          <p className="text-white animate-pulse">Loading...</p>
-        )}
-        <div />
+    <div className="relative bg-black">
+      <div className="absolute left-4 sm:left-8 top-3 sm:top-4 z-20">
+        <BackButton backPath="/admin/calendar" />
       </div>
-      <div className="flex gap-4">
-        <button
-          className="bg-white border-white p-4 border mt-4 rounded text-large flex items-center gap-4"
-          onClick={fetchImage}
-          style={{
-            marginTop: "20px",
-            padding: "10px 20px",
-            fontSize: "16px",
-            cursor: "pointer",
-          }}
-        >
-          <span>Regenerate</span>
-          {loading ? (
-            <AiOutlineLoading3Quarters size={20} className="animate-spin" />
+      <div className="relative min-h-screen flex flex-col items-center pt-20 md:pt-8">
+        <div className="h-[75vh] flex items-center align-middle">
+          {imageUrl ? (
+            <img
+              className="border border-white"
+              src={imageUrl}
+              alt="Schedule Artwork"
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
           ) : (
-            <TfiReload size={20} />
-          )}{" "}
-        </button>
-        {imageUrl && (
+            <p className="text-white animate-pulse">Loading...</p>
+          )}
+          <div />
+        </div>
+        <div className="flex gap-4">
           <button
-            className="bg-white border-white p-4 border mt-4 rounded text-large gap-4"
-            onClick={downloadImage}
+            className="bg-white border-white p-4 border mt-4 rounded text-large flex items-center gap-4"
+            onClick={fetchImage}
             style={{
               marginTop: "20px",
               padding: "10px 20px",
@@ -86,9 +63,28 @@ const ScheduleArtworkPage: NextPage & {
               cursor: "pointer",
             }}
           >
-            Download
+            <span>Regenerate</span>
+            {loading ? (
+              <AiOutlineLoading3Quarters size={20} className="animate-spin" />
+            ) : (
+              <TfiReload size={20} />
+            )}{" "}
           </button>
-        )}
+          {imageUrl && (
+            <button
+              className="bg-white border-white p-4 border mt-4 rounded text-large gap-4"
+              onClick={downloadImage}
+              style={{
+                marginTop: "20px",
+                padding: "10px 20px",
+                fontSize: "16px",
+                cursor: "pointer",
+              }}
+            >
+              Download
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
