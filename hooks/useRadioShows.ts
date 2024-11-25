756deb3708fd60b882f1900ad685e2226b6bc235
Jack MB <hi@jackmurraybrown.com>
2024-11-25 12:09:37 +0100

    bugfix same key for fetch with guests and news page

--- hooks/useRadioShows.ts
+++ hooks/useRadioShows.ts
@@ -2,21 +2,24 @@ import useSWRInfinite from "swr/infinite";
 import { PastShowSchema } from "../types/shared";
 import { RADIO_SHOWS_PAGE_SIZE } from "../lib/contentful/pages/radio";
 
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the data.");
+  }
+  return await response.json();
+};
+
 export default function useRadioShows(
   fallbackData: PastShowSchema[],
   filter: string[]
 ) {
   const { data, setSize, error, isValidating, isLoading } = useSWRInfinite(
-    (pageIndex) => [pageIndex * RADIO_SHOWS_PAGE_SIZE, filter],
-    async (skip) => {
-      const r = await fetch(
-        `/api/shows?take=${RADIO_SHOWS_PAGE_SIZE}&skip=${skip}&filter=${encodeURIComponent(
-          filter.join(",")
-        )}`
-      );
-
-      return await r.json();
-    },
+    (pageIndex) =>
+      `/api/shows?take=${RADIO_SHOWS_PAGE_SIZE}&skip=${
+        pageIndex * RADIO_SHOWS_PAGE_SIZE
+      }&filter=${encodeURIComponent(filter.join(","))}`,
+    fetcher,
     {
       fallbackData: filter.length == 0 ? [fallbackData] : [],
       revalidateFirstPage: false,
