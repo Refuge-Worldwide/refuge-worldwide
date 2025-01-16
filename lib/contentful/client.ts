a9338d9d4ddf9d5fc3cb1a1f3313e8b20de17a09
Jack MB <hi@jackmurraybrown.com>
2025-01-16 14:53:22 +0100

    fix past show schema

--- lib/contentful/client.ts
+++ lib/contentful/client.ts
@@ -60,7 +60,7 @@ export const createPastShowSchema = (show: TypeShow): PastShowSchema => ({
   mixcloudLink: show.fields.mixcloudLink,
   coverImage: show.fields.coverImage.fields.file.url,
   genres: show.fields.genres.map((genre) => genre.fields?.name).filter(Boolean),
-  artwork: show.fields.artwork?.fields.file.url,
+  artwork: show.fields.artwork ? show.fields.artwork.fields.file.url : null,
 });
 
 export async function getPastShows(
