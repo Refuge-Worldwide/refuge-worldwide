b20d23fc7cb2230c0bccb735cba6a096909753a1
a.v <hi@jackmurraybrown.com>
2025-01-22 13:44:47 +0100

    fix image cropping yayyyyy (#165)

--- lib/loaders.ts
+++ lib/loaders.ts
@@ -2,7 +2,10 @@ import type { ImageLoader } from "next/image";
 
 const loaders: Record<string, ImageLoader> = {
   contentful: ({ src, width, quality = 75 }) => {
-    return `${src}?w=${width}&q=${quality}&fm=jpg&fl=progressive`;
+    // crop images to 16:9 aspect ratio so we don't chop any faces in half
+    const aspectRatio = 16 / 9;
+    const height = Math.round(width / aspectRatio);
+    return `${src}?w=${width}&h=${height}&q=${quality}&fm=jpg&fl=progressive&f=faces&fit=fill`;
   },
 };
 
