e427b557d4802841d66c64eea0108c4064b27ef9
Jack MB <hi@jackmurraybrown.com>
2025-04-24 16:45:17 +0200

    update types for upcoming show

--- lib/contentful/pages/radio.ts
+++ lib/contentful/pages/radio.ts
@@ -102,6 +102,7 @@ export async function getRadioPageSingle(slug: string, preview: boolean) {
 
 export type UpcomingShowType = Pick<
   ShowInterface,
+  | "sys"
   | "title"
   | "date"
   | "slug"
