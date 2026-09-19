cfc2cc89e148b6da80f496f4d38a2902996aa2e5
Jack MB <hi@jackmurraybrown.com>
2026-09-19 09:50:22 +0200

    add repeat to media session title

--- components/livePlayer.tsx
+++ components/livePlayer.tsx
@@ -95,7 +95,9 @@ export default function LivePlayer() {
   useEffect(() => {
     if ("mediaSession" in navigator && scheduleData?.ch1?.liveNow) {
       navigator.mediaSession.metadata = new MediaMetadata({
-        title: scheduleData.ch1.liveNow.title,
+        title:
+          scheduleData.ch1.liveNow.title +
+          (scheduleData.ch1.liveNow.repeat ? " (Repeat)" : ""),
         artist: "Refuge Worldwide",
         artwork: [
           {
